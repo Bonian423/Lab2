101017489fdb711059a6e78d7646f310bb802912
Bonian Pan <bonianp2@illinois.edu>
2018-09-12 16:13:44 -0500

    well, the methods don't exist yet

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,18 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while (!Maze.isFinished()) {
+                Maze.turnRight();
+                if (Maze.canMove()){
+                    Maze.move();
+                } else {
+                    while (!Maze.canMove()){
+                        Maze.turnLeft();
+                    }
+                    Maze.move();
+                }
+            }
+
         }
 
         if (maze.isFinished()) {
